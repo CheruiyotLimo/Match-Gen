8b74eaf872d4a696db8eb667fd535952b27fad6e
Yoni Limo <limzyon@gmail.com>
2023-01-29 23:41:31 +0300

    new trial

--- MatchGenerator/try.py
+++ MatchGenerator/try.py
@@ -1,28 +1,5 @@
-def collectStrings(obj):
-    lst = []
-    for key in obj:
-        if type(obj[key]) is str:
-            lst += [obj[key]]
-            # return lst
-        else:
-            if type(obj[key]) is dict:
-                return lst + collectStrings(obj[key])
-    return lst
+import random
 
-obj = {
-  "stuff": 'foo',
-  "data": {
-    "val": {
-      "thing": {
-        "info": 'bar',
-        "moreInfo": {
-          "evenMoreInfo": {
-            "weMadeIt": 'baz'
-          }
-        }
-      }
-    }
-  }
-}
- 
-print(collectStrings(obj)) # ['foo', 'bar', 'baz']
+aso = [1, 3, 5, 5, 2, 7, 9]
+print(type(random.shuffle(aso)))
+
