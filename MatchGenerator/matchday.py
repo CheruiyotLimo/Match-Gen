a41a7c1507b8bcb58d8a85a8f9187371cc34bd3a
Yoni Limo <limzyon@gmail.com>
2023-01-29 23:41:22 +0300

    Done

--- MatchGenerator/matchday.py
+++ MatchGenerator/matchday.py
@@ -1,4 +1,5 @@
 TEAMS = ["Arsenal", "Man Utd", "Man City", "Tottenham Hotspurs", "Chelsea", "Liverpool"]
+import random
 
 
 class Matchday:
@@ -7,7 +8,8 @@ class Matchday:
 
     def create_all_fixtures(self, teams=TEAMS):
         self.fixtures_all = [(team1, team2) for team1 in teams for team2 in teams if team1 != team2]
-        print(self.fixtures_all)
+        # print(self.fixtures_all)
+        # print(len(self.fixtures_all))
         return
 
     # @property
@@ -28,10 +30,11 @@ class Matchday:
     def individual_matchday_fixtures(self):
         '''All matches to be played on one MatchDay.'''
         matchdays = self.number_of_matchdays()
+        random.shuffle(self.fixtures_all)
         for md in matchdays:
             checker = []
-            while len(matchdays[md]) < self.number_of_fixtures_on_one_matchday():  # Need a condition to loop it back to find fixtures
-                for fixture in self.fixtures_all:                                  # that had been passed over the first time
+            while len(matchdays[md]) < self.number_of_fixtures_on_one_matchday():            # Need a condition to loop it back to find fixtures
+                for fixture in self.fixtures_all:                                           # that had been passed over the first time
                     team1, team2 = fixture
                     if team1 not in checker and team2 not in checker:
                         checker.append(team1)
@@ -40,10 +43,13 @@ class Matchday:
                         self.fixtures_all.remove(fixture)
                 break
             checker.clear()
-
+        if self.fixtures_all:
+            self.individual_matchday_fixtures()
+        # print(len(self.fixtures_all))
+        print(len(self.fixtures_all))
         return matchdays
 
-    def md_length_checker(self, day_int):  ##Thinking of using this as a decorator.
+    def md_length_checker(self, day_int):                                       #Thinking of using this as a decorator.
         '''Decorator for checking number of matchday matches.'''
         j = f"Matchday {day_int}"
         # print(j)
@@ -63,5 +69,5 @@ mt = Matchday()
 mt.create_all_fixtures()
 # print(mt.number_of_matchdays)
 # print(mt.number_of_fixtures_on_one_matchday())
-mt.md_length_checker(2)
+# mt.md_length_checker(2)
 print(mt.individual_matchday_fixtures())
